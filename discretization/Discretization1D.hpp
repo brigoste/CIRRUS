7937b7537283e62478f6f655b6520d1766c00357
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-05-06 15:36:53 -0700

    Update Discretization1D.hpp

--- discretization/Discretization1D.hpp
+++ discretization/Discretization1D.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include "mesh/Mesh1D.hpp"
-#include "coeffs/Coefficients1D.hpp"
+#include "system/LinearSystem.hpp"
+#include "system/HeatSystem1D.hpp"
 
 void discretize_1D(
     const Mesh1D& mesh,
+    LinearSystem& sys,
     double k,
-    double A,
-    Coefficients1D& c
+    double A
 );
