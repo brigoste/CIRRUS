0021a982778027257e7868babd4d30e34d90c532
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-05-06 15:32:13 -0700

    Create SolverMethod.hpp

---
+++ Solver/SolverMethod.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+
+enum class SolverMethod
+{
+    TDMA,
+    GS,
+    SOR
+};
+
+inline std::string to_string(SolverMethod m)
+{
+    switch (m)
+    {
+        case SolverMethod::TDMA: return "TDMA";
+        case SolverMethod::GS:   return "GS";
+        case SolverMethod::SOR:  return "SOR";
+        default: return "UNKNOWN";
+    }
+}
