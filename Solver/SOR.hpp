b7f81ebc1529b71aa26d1ccd14fe56639a2401ea
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-05-06 15:33:19 -0700

    Create SOR.hpp

---
+++ Solver/SOR.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "system/LinearSystem.hpp"
+#include <vector>
+
+std::vector<double> SOR(
+    LinearSystem& sys,
+    int iter = 1000,
+    double tol = 1e-5,
+    double omega = 1.2
+);
