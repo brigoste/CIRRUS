2479eadba741d80ae3d40d2d9f6ada6959273fc7
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-05-06 15:36:40 -0700

    Update Discretization1D.cpp

--- discretization/Discretization1D.cpp
+++ discretization/Discretization1D.cpp
@@ -2,17 +2,19 @@
 
 void discretize_1D(
     const Mesh1D& mesh,
+    LinearSystem& sys,
     double k,
-    double A,
-    Coefficients1D& c
-) {
-    for (int i = 1; i < mesh.n; i++) {
-        double aW = k*A/mesh.dx;
-        double aE = (i == mesh.n-1 ? 0.0 : k*A/mesh.dx);
+    double A
+)
+{
+    double dx = mesh.dx;
+    double a = k * A / dx;
 
-        c.aW[i] = aW;
-        c.aE[i] = aE;
-        c.aP[i] = aW + aE - c.Sp[i];
-        c.b[i]  = 0.0 + c.Su[i];
+    for (int i = 1; i < mesh.n - 1; i++)
+    {
+        sys.aW[i] = -a;
+        sys.aE[i] = -a;
+        sys.aP[i] = a + a - sys.Sp[i];
+        sys.b[i]  = sys.Su[i];
     }
 }
