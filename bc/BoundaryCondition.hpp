cce41f24e3337213dc4ac931af64f7c3ba6a4fce
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-05-06 15:34:28 -0700

    Update BoundaryCondition.hpp

--- bc/BoundaryCondition.hpp
+++ bc/BoundaryCondition.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "mesh/Mesh1D.hpp"
-#include "coeffs/Coefficients1D.hpp"
+#include "system/LinearSystem.hpp"
 
 class BoundaryCondition {
 public:
     virtual ~BoundaryCondition() = default;
 
     virtual void apply(const Mesh1D& mesh,
-                       Coefficients1D& c) const = 0;
+                       LinearSystem& sys,
+                       double k,
+                       double A) const = 0;
 };
