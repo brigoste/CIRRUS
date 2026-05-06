213debf0a10cc5eaeabadc4c28e0a95390c91ff7
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-05-06 15:31:42 -0700

    Update TDMA.hpp

--- Solver/TDMA.hpp
+++ Solver/TDMA.hpp
@@ -1,5 +1,6 @@
 #pragma once
-#include "coeffs/Coefficients1D.hpp"
+
+#include "system/LinearSystem.hpp"
 #include <vector>
 
-std::vector<double> TDMA(const Coefficients1D& c);
+std::vector<double> TDMA(const LinearSystem& sys);
