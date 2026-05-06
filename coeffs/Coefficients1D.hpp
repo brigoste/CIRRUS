9aa08b6727ba5bea618ed427eb5177e1e9ad3882
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-05-06 15:36:24 -0700

    Delete coeffs directory

--- coeffs/Coefficients1D.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-
-struct Coefficients1D {
-    std::vector<double> aP, aE, aW, b;
-    explicit Coefficients1D(int n);
-    std::vector<double> Su;
-    std::vector<double> Sp;
-};
