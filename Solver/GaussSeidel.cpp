c003cb42cfbf0fe1d16be10cb6a6dc77e2a31fc5
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-05-07 10:59:50 -0700

    Update GaussSeidel.cpp
    Added output for iterations

--- Solver/GaussSeidel.cpp
+++ Solver/GaussSeidel.cpp
@@ -6,17 +6,20 @@
 std::vector<double> GaussSeidel(
     LinearSystem& sys,
     int iter,
-    double tol
+    double tol,
+    bool output
 )
 {
     const int N = sys.aP.size();
 
     std::fill(sys.x.begin(), sys.x.end(), 0.0);
     std::fill(sys.x_old.begin(), sys.x_old.end(), 0.0);
+    int n_iter = 0;
 
     for (int it = 0; it < iter; ++it)
     {
         double maxDiff = 0.0;
+        n_iter++;
 
         for (int i = 0; i < N; ++i)
         {
@@ -45,8 +48,19 @@ std::vector<double> GaussSeidel(
             sys.x_old[i] = sys.x[i]; // keep sync
         }
 
-        if (maxDiff < tol)
+        if (maxDiff < tol){
+            if(output)
+                std::cout << "Hit tolerance." << std::endl;
             break;
+        }
+    }
+    if(output){
+        if(n_iter < iter){
+            std::cout << "Exited after " << n_iter << " iterations." << std::endl; 
+        }
+        else{
+            std::cout << "Exited after maximum iterations." << std::endl; 
+        }
     }
 
     return sys.x;
