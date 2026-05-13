b8a23fc569491ac827d276383a72e43149a30ca9
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-05-12 21:43:09 -0700

    Update HeatSystem1D.cpp

--- system/HeatSystem1D.cpp
+++ system/HeatSystem1D.cpp
@@ -1,52 +1,145 @@
-#include "HeatSystem1D.hpp"
-#include "discretization/Discretization1D.hpp"
-#include <functional>
-#include "Solver/SolverMethod.hpp"
-#include <iostream>
+#include "system/HeatSystem1D.hpp"
 
+#include "discretization/FiniteVolumeOperator.hpp"
 
-HeatSystem1D::HeatSystem1D(int n, double L, double A, double k, bool output)
-    : mesh_(n, L),
-      sys_(n),
-      A_(A),
-      k_(k),
-      output_(output)
+#include "Solver/TDMA.hpp"
+#include "Solver/GaussSeidel.hpp"
+#include "Solver/SOR.hpp"
+#include "Solver/CG.hpp"
+
+#include "bc/BoundaryConditionDescriptor.hpp"
+#include "physics/HeatEquationModel.hpp"
+
+#include <stdexcept>
+
+// -----------------------------------------
+// Constructor
+// -----------------------------------------
+HeatSystem1D::HeatSystem1D(const MeshBase& mesh,
+                           const HeatCase1D& problem)
+    : mesh_(mesh),
+      sys_(mesh.size()),
+      problem_(problem)
 {}
+
+// -----------------------------------------
+// BC application
+// -----------------------------------------
+void HeatSystem1D::applyDirichlet(const BoundaryConditionDescriptor& bc,
+                                  const BoundaryContext& ctx)
+{
+    int i = ctx.node;
+
+    sys_.clearRow(i);
+    sys_.addDiag(i, 1.0);
+    sys_.setRHS(i, bc.value);
+}
+
+void HeatSystem1D::applyNeumann(const BoundaryConditionDescriptor& bc,
+                                const BoundaryContext& ctx)
+{
+    int i = ctx.node;
+    sys_.addRHS(i, bc.flux * ctx.area);
+}
+
+void HeatSystem1D::applyConvective(const BoundaryConditionDescriptor& bc,
+                                   const BoundaryContext& ctx)
+{
+    int i = ctx.node;
+
+    sys_.addDiag(i, bc.h * ctx.area);
+    sys_.addRHS(i, bc.h * bc.Tinf * ctx.area);
+}
+
+// -----------------------------------------
+// Assembly
+// -----------------------------------------
 void HeatSystem1D::assemble()
 {
-    sys_.reset();
+    HeatEquationModel model;
+    model.k  = problem_.k;
+    model.Su = problem_.Su;
+    model.Sp = problem_.Sp;
 
-    discretize_1D(mesh_, sys_, k_, A_);
+    // -------------------------
+    // interior
+    // -------------------------
+    FiniteVolumeOperator::assemble(mesh_, model, sys_);
 
-    for (const auto& bc : bcs_)
+    // -------------------------
+    // boundary faces (NEW)
+    // -------------------------
+    for (const auto& bc : problem_.bcs)
     {
-        bc->apply(mesh_, sys_, k_, A_);
+        BoundaryContext ctx = mesh_.boundaryContext(bc.face);
+
+        switch (bc.type)
+        {
+            case BCType::Dirichlet:
+                applyDirichlet(bc, ctx);
+                break;
+
+            case BCType::Neumann:
+                applyNeumann(bc, ctx);
+                break;
+
+            case BCType::Convective:
+                applyConvective(bc, ctx);
+                break;
+        }
     }
 }
-void HeatSystem1D::setSource(
-    std::function<double(double)> Su_func,
-    std::function<double(double)> Sp_func)
-{
-    sys_.setSource(Su_func, Sp_func, mesh_.x);
-}
-void HeatSystem1D::addBC(std::unique_ptr<BoundaryCondition> bc)
-{
-    bcs_.push_back(std::move(bc));
-}
-std::vector<double> HeatSystem1D::solve(SolverMethod method, int iter, double tol, double omega)
-{
-    // std::cout << "Entering solve()\n";
 
+// -----------------------------------------
+// Solve
+// -----------------------------------------
+std::vector<double> HeatSystem1D::solve(
+    SolverMethod method,
+    int max_iter,
+    double tol,
+    double omega)
+{
     switch (method)
     {
-        case SolverMethod::TDMA:
-            return TDMA(sys_);
+        case SolverMethod::CG:
+            return CG(sys_, max_iter, tol);
+
         case SolverMethod::GS:
-            return GaussSeidel(sys_, iter, tol, output_);
+            return GaussSeidel(sys_, mesh_, max_iter, tol, false);
+
         case SolverMethod::SOR:
-            return SOR(sys_, iter, tol, omega, output_);
+            return SOR(sys_, mesh_, max_iter, tol, omega, false);
+
+        case SolverMethod::TDMA:
+            return TDMA(sys_, mesh_.size(), false);
+
         default:
             throw std::runtime_error("Unknown solver method");
     }
-
 }
+
+// -----------------------------------------
+// Interface methods
+// -----------------------------------------
+const MeshBase& HeatSystem1D::mesh() const
+{
+    return mesh_;
+}
+
+const LinearSystem& HeatSystem1D::system() const
+{
+    return sys_;
+}
+
+int HeatSystem1D::size() const
+{
+    return sys_.size();
+}
+
+// -----------------------------------------
+// Polymorphic BC interface (currently unused path)
+// -----------------------------------------
+// void HeatSystem1D::addBC(std::unique_ptr<BoundaryCondition> bc)
+// {
+//     bcs_.push_back(std::move(bc));
+// }
