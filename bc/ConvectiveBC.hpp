ce85053c42a48bfe8d46f704fa954363628e4737
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-05-12 21:26:18 -0700

    Update ConvectiveBC.hpp

--- bc/ConvectiveBC.hpp
+++ bc/ConvectiveBC.hpp
@@ -1,14 +1,16 @@
 #pragma once
+
 #include "BoundaryCondition.hpp"
 
-class ConvectiveBC : public BoundaryCondition {
+class ConvectiveBC : public BoundaryCondition
+{
     public:
-        ConvectiveBC(int face, double h, double Tinf);
+        ConvectiveBC(double h, double Tinf);
 
-        void apply(const Mesh1D& m, LinearSystem& sys, double k, double A) const override;
+        void apply(LinearSystem& sys,
+                const BoundaryContext& ctx) const override;
 
     private:
-        int face_;
         double h_;
         double Tinf_;
 };
