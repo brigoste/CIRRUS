2f4261aa6683d89d0d42d7b0d28135d2ef68bc44
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-05-14 21:33:51 -0700

    Update FiniteVolumeOperator.hpp

--- discretization/FiniteVolumeOperator.hpp
+++ discretization/FiniteVolumeOperator.hpp
@@ -3,6 +3,10 @@
 #include "mesh/MeshBase.hpp"
 #include "linear_system/LinearSystem.hpp"
 #include "physics/HeatEquationModel.hpp"
+#include "discretization/FluxAccumulator.hpp"
+#include "discretization/CellFlux.hpp"
+#include "discretization/CellData.hpp"
+#include "bc/BCType.hpp"
 
 class FiniteVolumeOperator
 {
