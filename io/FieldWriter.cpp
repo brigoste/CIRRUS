ca52c44c8108db0c80f187d4e7c1773b729618a0
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-05-14 21:37:40 -0700

    Update FieldWriter.cpp

--- io/FieldWriter.cpp
+++ io/FieldWriter.cpp
@@ -1,32 +1,52 @@
 #include "io/FieldWriter.hpp"
+#include "mesh/MeshBase.hpp"
+#include "linear_system/LinearSystem.hpp"
 #include <fstream>
-#include <iostream>
+#include <stdexcept>
 
-void FieldWriter::writeCSV(
+void FieldWriter::writeCSVDebug(
     const MeshBase& mesh,
-    const std::vector<double>& field,
+    const std::vector<double>& phi,
+    const std::vector<double>& rhs,
+    const std::vector<double>& residual,
     const std::string& filename)
 {
     std::ofstream f(filename);
-    std::cout << "Writing data to " << filename << std::endl;
 
     if (!f.is_open())
+        throw std::runtime_error("Failed to open debug CSV");
+
+    const std::size_t N = mesh.ncells();
+
+    Point p0 = mesh.cellCenter(0);
+
+    if (p0.x[1] == 0.0 && p0.x[2] == 0.0)
     {
-        std::cerr << "FAILED to open file: " << filename << std::endl;
-        return;
+        f << "x,phi,rhs,residual\n";
+
+        for (std::size_t i = 0; i < N; ++i)
+        {
+            Point p = mesh.cellCenter(i);
+
+            f << p.x[0] << ","
+              << phi[i] << ","
+              << rhs[i] << ","
+              << residual[i] << "\n";
+        }
     }
-
-    f << "x,y,z,value\n";
-
-    for (int i = 0; i < mesh.size(); i++)
+    else
     {
-        auto p = mesh.point(i);
+        f << "x,y,phi,rhs,residual\n";
 
-        double x = (p.x.size() > 0) ? p.x[0] : 0.0;
-        double y = (p.x.size() > 1) ? p.x[1] : 0.0;
-        double z = (p.x.size() > 2) ? p.x[2] : 0.0;
+        for (std::size_t i = 0; i < N; ++i)
+        {
+            Point p = mesh.cellCenter(i);
 
-        f << x << "," << y << "," << z << "," << field[i] << "\n";
-        // std::cout << x << "," << y << "," << z << "," << field[i] << "\n";
+            f << p.x[0] << ","
+              << p.x[1] << ","
+              << phi[i] << ","
+              << rhs[i] << ","
+              << residual[i] << "\n";
+        }
     }
 }
