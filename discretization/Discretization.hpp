dbf8c4da7deeb3b20dd9bdf0f27d49ae15be43c3
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-02 08:31:31 -0700

    Update Discretization.hpp

--- discretization/Discretization.hpp
+++ discretization/Discretization.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "mesh/MeshBase.hpp"
+#include "mesh/Face.hpp"
 #include "linear_system/LinearSystem.hpp"
+#include "mesh/BoundaryPatchSystem.hpp"
 
 // Generic finite-volume diffusion discretization
 void discretize(
     const MeshBase& mesh,
+    const BoundaryPatchSystem& bcSystem,
     LinearSystem& sys,
-    double k
-);
+    double k);
 
