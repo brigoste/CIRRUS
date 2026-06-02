44ab1996b55130e3aa5041fd6179a9a596e9d2d2
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-02 08:50:28 -0700

    Update GaussSeidel.hpp

--- Solver/GaussSeidel.hpp
+++ Solver/GaussSeidel.hpp
@@ -1,13 +1,8 @@
 #pragma once
-
 #include "linear_system/LinearSystem.hpp"
-#include "mesh/MeshBase.hpp"
 #include <vector>
 
 std::vector<double> GaussSeidel(
-    LinearSystem& sys,
-    [[maybe_unused]]const MeshBase& mesh,
-    int iter = 1000,
-    double tol = 1e-8,
-    bool output = false
-);
+    const LinearSystem sys,
+    int max_iter,
+    double tol);
