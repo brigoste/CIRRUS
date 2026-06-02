65f88341ed96d54754295206c30b4ef768c4460b
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-02 08:51:46 -0700

    Update SOR.hpp

--- Solver/SOR.hpp
+++ Solver/SOR.hpp
@@ -1,14 +1,9 @@
 #pragma once
-
 #include "linear_system/LinearSystem.hpp"
-#include "mesh/MeshBase.hpp"
 #include <vector>
 
 std::vector<double> SOR(
-    LinearSystem& sys,
-    [[maybe_unused]]const MeshBase& mesh,
-    int iter = 1000,
-    double tol = 1e-8,
-    double omega = 1.2,
-    bool output = false
-);
+    const LinearSystem& sys,
+    int max_iter,
+    double tol,
+    double omega);
