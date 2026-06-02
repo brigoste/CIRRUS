7606bba9ae6946141b265c80faaf4240292236db
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-02 08:42:33 -0700

    Update MeshBase.hpp

--- mesh/MeshBase.hpp
+++ mesh/MeshBase.hpp
@@ -1,45 +1,32 @@
 #pragma once
-
 #include <cstddef>
-#include <limits>
 #include <vector>
-#include <optional>
 #include "mesh/Point.hpp"
-// #include "bc/BoundaryConditionDescriptor.hpp"
-// #include "bc/BoundaryContext.hpp"
-#include "mesh/BoundaryPatchSystem.hpp"
 #include "mesh/Face.hpp"
 
-static constexpr std::size_t INVALID = std::numeric_limits<std::size_t>::max();
-
-class MeshBase
-{
+class MeshBase {
 public:
     virtual ~MeshBase() = default;
 
     // topology
+    virtual std::size_t dim() const = 0;
     virtual std::size_t ncells() const = 0;
     virtual std::size_t nnodes() const = 0;
+    virtual std::size_t nfaces() const = 0;
 
     // geometry
-    virtual Point cellCenter(std::size_t i) const = 0;
-    virtual double cellVolume(std::size_t i) const = 0;
+    virtual const Point& node(std::size_t i) const = 0;
+    virtual const Point& cellCenter(std::size_t c) const = 0;
 
-    // nodes
-    virtual Point node(std::size_t i) const = 0;
-    virtual std::size_t cellNodeCount(std::size_t i) const = 0;
-    virtual std::size_t cellNode(std::size_t c, std::size_t k) const = 0;
+    // connectivity (canonical)
+    virtual void cellNodes(
+        std::size_t c,
+        std::vector<std::size_t>& nodes) const = 0;
+
+    virtual int vtkCellType(std::size_t c) const = 0;
 
     // faces
-    virtual std::size_t nFaces() const = 0;
     virtual const Face& face(std::size_t f) const = 0;
-    
-    virtual std::size_t dim() const = 0;
-
-    const std::vector<std::size_t>& cellFaces(std::size_t c) const;
-    // const std::vector<Face>& faces() const;
-
-    // NEW: iterator-style access
     virtual std::vector<Face>::const_iterator facesBegin() const = 0;
     virtual std::vector<Face>::const_iterator facesEnd() const = 0;
 };
