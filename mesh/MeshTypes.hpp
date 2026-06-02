c5220d05f78dcb19a688b4d7ad3cbb3499b7d463
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-02 08:42:59 -0700

    Create MeshTypes.hpp

---
+++ mesh/MeshTypes.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <cstddef>
+#include <limits>
+
+struct MeshTypes
+{
+    using index_type = std::size_t;
+
+    static constexpr index_type INVALID =
+        std::numeric_limits<index_type>::max();
+};
