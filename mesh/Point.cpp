1cd9e71b18a0623e3d8086fa0325ec16d52c52a8
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-03 16:04:43 -0700

    Update Point.cpp
    Added struct

--- mesh/Point.cpp
+++ mesh/Point.cpp
@@ -1,4 +1,15 @@
-#include "mesh/Point.hpp"
+#pragma once
+#include <array>
+
+struct Point
+{
+    std::array<double, 3> x{};  // fixed max dimension
+
+    double& operator[](std::size_t i) { return x[i]; }
+    double  operator[](std::size_t i) const { return x[i]; }
+
+    std::size_t dim() const { return 3; }
+};
 
 inline Point operator+(const Point& a, const Point& b)
 {
