7b837195ae26c2c0723b3dfcc11b3101c8d49546
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-03 16:01:23 -0700

    Update Mesh1D.cpp
    Updated use of face

--- mesh/Mesh1D.cpp
+++ mesh/Mesh1D.cpp
@@ -1,5 +1,9 @@
 #include "mesh/Mesh1D.hpp"
+#include "mesh/Point.hpp"
+#include "utils/LinearAlgebraUtils.hpp"
 #include <stdexcept>
+#include <cassert>
+#include <cmath>
 
 // ===============================
 // Constructor
@@ -27,32 +31,42 @@ Mesh1D::Mesh1D(std::size_t N, double L)
         Face& face = faces_[f];
 
         face.center.x[0] = f * dx_;
-        face.center.x[1] = 0.0;
-        face.center.x[2] = 0.0;
-
-        face.normal.x[0] = 1.0;
-        face.normal.x[1] = 0.0;
-        face.normal.x[2] = 0.0;
-
         face.area = 1.0;
-        face.d = dx_;
 
         if (f == 0)
         {
             face.owner = 0;
             face.neighbor = Face::INVALID;
             face.normal.x[0] = -1.0;
+
+            face.dPN = 0.0;
         }
         else if (f == N_)
         {
             face.owner = N_ - 1;
             face.neighbor = Face::INVALID;
+            face.normal.x[0] = 1.0;
+
+            face.dPN = 0.0;
         }
         else
         {
             face.owner = f - 1;
             face.neighbor = f;
+            face.normal.x[0] = 1.0;
+
+            // THIS is now the ONLY place interior spacing is defined
+            face.dPN = dx_;
         }
+    }
+
+    if (faces_.size() != N_ + 1)
+        throw std::runtime_error("Invalid 1D mesh topology");
+    
+    for (std::size_t f = 1; f < N_; ++f)
+    {
+        assert(faces_[f].owner == f - 1);
+        assert(faces_[f].neighbor == f);
     }
 }
 
@@ -72,10 +86,16 @@ const Face& Mesh1D::face(std::size_t f) const { return faces_.at(f); }
 std::vector<Face>::const_iterator Mesh1D::facesBegin() const { return faces_.begin(); }
 std::vector<Face>::const_iterator Mesh1D::facesEnd() const { return faces_.end(); }
 
-// =========================================================
-// NEW: VTK + connectivity interface
-// =========================================================
-
+double Mesh1D::faceDistance(std::size_t f) const
+{
+    (void) f;
+    return dx_;
+}
+double Mesh1D::distance(const Point& a, const Point& b) const
+{
+    double dx = a[0] - b[0];
+    return std::abs(dx);
+}
 void Mesh1D::cellNodes(std::size_t c, std::vector<std::size_t>& nodes) const
 {
     nodes = {c, c + 1};
