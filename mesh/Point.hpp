786f5e893cf3ea366a5a3784b0065a76d40158cb
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-03 16:05:15 -0700

    Update Point.hpp -> overwrite Point.cpp

--- mesh/Point.hpp
+++ mesh/Point.hpp
@@ -11,6 +11,35 @@ struct Point
     std::size_t dim() const { return 3; }
 };
 
-inline Point operator+(const Point& a, const Point& b);
-inline Point operator-(const Point& a, const Point& b);
-inline Point operator*(double s, const Point& a);
+inline Point operator+(const Point& a, const Point& b)
+{
+    return Point{
+        {
+            a.x[0] + b.x[0],
+            a.x[1] + b.x[1],
+            a.x[2] + b.x[2]
+        }
+    };
+}
+
+inline Point operator-(const Point& a, const Point& b)
+{
+    return Point{
+        {
+            a.x[0] - b.x[0],
+            a.x[1] - b.x[1],
+            a.x[2] - b.x[2]
+        }
+    };
+}
+
+inline Point operator*(double s, const Point& a)
+{
+    return Point{
+        {
+            s * a.x[0],
+            s * a.x[1],
+            s * a.x[2]
+        }
+    };
+}
