36796e6929defe6b23ae3cd35f3c402b48c217f1
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-03 16:00:38 -0700

    Update Face.hpp
    Replace d with dPn

--- mesh/Face.hpp
+++ mesh/Face.hpp
@@ -1,16 +1,19 @@
 #pragma once
-#include "mesh/MeshTypes.hpp"
 #include "mesh/Point.hpp"
+#include <cstddef>
 
-struct Face {
+struct Face
+{
     std::size_t owner;
-    std::size_t neighbor;   // INVALID = boundary
+    std::size_t neighbor;
 
     Point center;
     Point normal;
 
-    double area;
-    double d;
+    double area = 1.0;
+
+    // geometric distance between owner and neighbor cell centers (INTERIOR ONLY)
+    double dPN = 0.0;
 
     static constexpr std::size_t INVALID = static_cast<std::size_t>(-1);
 };
