ee7fd568b9142a03992877decd762a4e820ba2b0
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-03 15:53:41 -0700

    Create PhysicsFactory.hpp

---
+++ physics/PhysicsFactory.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <memory>
+#include "physics/PhysicsModel.hpp"
+#include "config/SimulationConfig.hpp"
+
+class PhysicsFactory
+{
+public:
+    static std::unique_ptr<PhysicsModel>
+    create(const PhysicsConfig& cfg);
+};
