9142f21b2938c2c3a41f23e111c3e1cba53b68d1
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-03 15:51:56 -0700

    Delete physics/HeatEquationModel.cpp

--- physics/HeatEquationModel.cpp
+++
@@ -1,9 +0,0 @@
-#include "physics/HeatEquationModel.hpp"
-
-HeatEquationModel makeConstantSource(double su, double sp)
-{
-    HeatEquationModel m;
-    m.Su = [=](const Point&) { return su; };
-    m.Sp = [=](const Point&) { return sp; };
-    return m;
-}
