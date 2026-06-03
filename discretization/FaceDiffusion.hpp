7809f2d54b22c97b683ebcffe96f1af2f3ca11e8
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-03 15:55:45 -0700

    Update FaceDiffusion.hpp
    Redefined how we handle faces.

--- discretization/FaceDiffusion.hpp
+++ discretization/FaceDiffusion.hpp
@@ -1,8 +1,16 @@
 #pragma once
 #include <stdexcept>
 
+enum class FaceType
+{
+    Interior,
+    Boundary
+};
+
 struct FaceDiffusion
 {
-    std::size_t P, N;
+    std::size_t P;
+    std::size_t N;     // only valid if Interior
     double D;
+    FaceType type;
 };
