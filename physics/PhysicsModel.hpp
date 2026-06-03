73d22e45f50397da4b0eba40aae208211afbb6a1
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-03 15:54:01 -0700

    Create PhysicsModel.hpp

---
+++ physics/PhysicsModel.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "mesh/Point.hpp"
+#include "mesh/Face.hpp"
+#include "mesh/MeshBase.hpp"
+#include "discretization/FluxAccumulator.hpp"
+
+class PhysicsModel
+{
+public:
+    virtual ~PhysicsModel() = default;
+
+    virtual double diffusionCoeff(const Face&, double d) const = 0;
+    virtual double convectionCoeff(double) const = 0;
+
+    // NEW: boundary closure hooks
+    virtual double wallConductivity() const = 0;
+    virtual double reconstructBoundaryValue(const BoundaryPatchSystem::Condition& bc,
+                                            double phiCell,
+                                            double dx,
+                                            bool isLeft) const = 0;
+    virtual void addCellSources(const MeshBase& mesh, 
+                                std::size_t cell,
+                                FluxAccumulator& flux) const
+    {
+        // Can add source term per cell here. Only treat a single cell here as we loop in FluxBuilder.cpp 
+        (void)mesh;
+        (void)cell;
+        (void)flux;
+    }
+    
+};
