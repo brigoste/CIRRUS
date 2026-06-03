b609aa31967760e63807eba96723dc11090d439e
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-03 16:03:03 -0700

    Create MeshGeometry.hpp
    Handles geometric numerics between nodes/cells.

---
+++ mesh/MeshGeometry.hpp
@@ -0,0 +1,23 @@
+#include "mesh/MeshGeometry.hpp"
+#include "mesh/MeshBase.hpp"
+#include "mesh/Face.hpp"
+
+double MeshGeometry::cellDistance(const MeshBase& mesh,
+                                   std::size_t P,
+                                   std::size_t N)
+{
+    const auto& xP = mesh.cellCenter(P);
+    const auto& xN = mesh.cellCenter(N);
+
+    return std::abs(xN[0] - xP[0]);
+}
+
+double MeshGeometry::faceDistance(const MeshBase& mesh,
+                                   const Face& face,
+                                   std::size_t P)
+{
+    const auto& xP = mesh.cellCenter(P);
+    const auto& xf = face.center;
+
+    return std::abs(xf[0] - xP[0]);
+}
