ba39b72f7753e95ca51359681c4716146a80d93e
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-08 09:57:17 -0700

    Update FieldWriter.cpp

--- io/FieldWriter.cpp
+++ io/FieldWriter.cpp
@@ -1,12 +1,9 @@
 #include "io/FieldWriter.hpp"
-#include "mesh/MeshBase.hpp"
-#include "linear_system/LinearSystem.hpp"
 #include <fstream>
 #include <stdexcept>
-#include <cmath>
 
 void FieldWriter::writeCSVDebug(
-    const Field1D& field,
+    const PointField& field,
     const std::vector<double>& rhs,
     const std::vector<double>& residual,
     const std::string& filename)
@@ -16,40 +13,25 @@ void FieldWriter::writeCSVDebug(
     if (!f.is_open())
         throw std::runtime_error("Failed to open debug CSV");
 
-    f << "x,phi,rhs,residual\n";
+    f << "x,y,z,phi,rhs,residual\n";
 
     const std::size_t N = field.phi.size();
 
     for (std::size_t i = 0; i < N; ++i)
     {
-        if (i == 0 || i == N-1) {
-            f << field.x[i] << "," << field.phi[i] << "," << 0 << "," << residual[i] << "\n";
-        }
-        else {
-            f << field.x[i] << "," << field.phi[i] << "," << rhs[i-1] << "," << residual[i] << "\n";
-        }
+        const Point& p = field.x[i];
+
+        const double rhs_i =
+            (i < rhs.size()) ? rhs[i] : 0.0;
+
+        const double res_i =
+            (i < residual.size()) ? residual[i] : 0.0;
+
+        f << p.x[0] << ","
+          << p.x[1] << ","
+          << p.x[2] << ","
+          << field.phi[i] << ","
+          << rhs_i << ","
+          << res_i << "\n";
     }
 }
-
-// void FieldWriter::writeCSVDebug(
-//     const Field1D& field,
-//     const std::vector<double>& rhs,
-//     const std::vector<double>& residual,
-//     const std::string& filename)
-// {
-//     if (field.x.size() != field.phi.size())
-//         throw std::runtime_error("Field1D size mismatch");
-
-//     std::ofstream file(filename);
-
-//     file << "x,phi,rhs,residual\n";
-
-//     for (std::size_t i = 0; i < field.x.size(); ++i)
-//     {
-//         file << field.x[i] << ","
-//              << field.phi[i] << ","
-//              << (i < rhs.size() ? rhs[i] : 0.0) << ","
-//              << (i < residual.size() ? residual[i] : 0.0)
-//              << "\n";
-//     }
-// }
