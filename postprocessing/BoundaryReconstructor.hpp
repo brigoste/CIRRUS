0fc3337dff771d50e1e1e7f9298ea2c387b1eed3
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-08 10:15:36 -0700

    Update BoundaryReconstructor.hpp

--- postprocessing/BoundaryReconstructor.hpp
+++ postprocessing/BoundaryReconstructor.hpp
@@ -1,24 +1,19 @@
 #pragma once
 
-#include "postprocessing/Field1D.hpp"
 #include "mesh/MeshBase.hpp"
 #include "mesh/BoundaryPatchSystem.hpp"
 #include "mesh/MeshGeometry.hpp"
 #include "physics/PhysicsModel.hpp"
-
-struct BoundaryField1D
-{
-    double x;
-    double phi;
-};
+#include "io/PointField.hpp"
 
 class BoundaryReconstructor
 {
 public:
 
-    static Field1D reconstruct(
+    static PointField reconstruct(
         const MeshBase& mesh,
         const BoundaryPatchSystem& boundary,
         const PhysicsModel& model,
         const std::vector<double>& phi);
+
 };
