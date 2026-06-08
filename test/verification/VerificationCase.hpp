8a52d2952f426626c7f89dced696d8815d99848f
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-08 10:21:11 -0700

    Create VerificationCase.hpp

---
+++ test/verification/VerificationCase.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+// Abstraction for all tests we use to verify.
+
+class VerificationCase
+{
+public:
+    virtual ~VerificationCase() = default;
+
+    // Exact solution φ(x,y)
+    virtual double exact(
+        double x,
+        double y) const = 0;
+
+    // Source term S(x,y)
+    virtual double source(
+        double x,
+        double y) const = 0;
+};
