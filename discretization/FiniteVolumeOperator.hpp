f44de2fdba06966f5e1aafb32578f06a783fa5bb
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-08 09:56:02 -0700

    Update FiniteVolumeOperator.hpp

--- discretization/FiniteVolumeOperator.hpp
+++ discretization/FiniteVolumeOperator.hpp
@@ -1,16 +1,19 @@
 #pragma once
-#include "discretization/FluxAccumulator.hpp"
-#include "mesh/MeshBase.hpp"
-#include "mesh/BoundaryPatchSystem.hpp"
-#include "physics/PhysicsModel.hpp"
-#include "utils/LinearAlgebraUtils.hpp"
 
-class FluxBuilder
+#include "mesh/MeshBase.hpp"
+#include "linear_system/LinearSystem.hpp"
+#include "physics/PhysicsModel.hpp"
+#include "discretization/FluxAccumulator.hpp"
+#include "discretization/CellFlux.hpp"
+#include "discretization/CellData.hpp"
+#include "bc/BCType.hpp"
+
+// #pragma message("USING FV HEADER: " __FILE__)
+
+class FiniteVolumeOperator
 {
 public:
-    static void buildFlux(
-        const MeshBase& mesh,
-        const PhysicsModel& model,
-        const BoundaryPatchSystem& boundary,
-        FluxAccumulator& flux);
+    static void assemble(
+        const FluxAccumulator& flux,
+        LinearSystem& sys);
 };
