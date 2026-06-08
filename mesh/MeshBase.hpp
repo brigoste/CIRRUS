3f3cdccea9e0d0504c231882bd85ba5bb64aea85
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-08 10:01:16 -0700

    Update MeshBase.hpp

--- mesh/MeshBase.hpp
+++ mesh/MeshBase.hpp
@@ -1,8 +1,10 @@
 #pragma once
+
 #include <cstddef>
 #include <vector>
-#include "mesh/Point.hpp"
-#include "mesh/Face.hpp"
+#include "mesh/primitives/Point.hpp"
+#include "mesh/primitives/Face.hpp"
+#include "mesh/primitives/Cell.hpp"
 
 class MeshBase {
 public:
@@ -21,9 +23,7 @@ public:
     virtual double distance(const Point& a, const Point& b) const = 0;
 
     // connectivity (canonical)
-    virtual void cellNodes(
-        std::size_t c,
-        std::vector<std::size_t>& nodes) const = 0;
+    virtual void cellNodes(std::size_t c, std::vector<std::size_t>& nodes) const = 0;
 
     virtual int vtkCellType(std::size_t c) const = 0;
 
@@ -32,4 +32,13 @@ public:
     virtual std::vector<Face>::const_iterator facesBegin() const = 0;
     virtual std::vector<Face>::const_iterator facesEnd() const = 0;
 
+    // virtual std::vector<std::size_t> boundaryFaces() const = 0;
+    virtual const std::vector<std::size_t>& boundaryFaces(std::size_t group) const = 0;
+    virtual std::size_t nBoundaryGroups() const = 0;
+
+    // cells
+    virtual const Cell& cell(std::size_t i) const = 0;
+
+    // for use in test case validation
+    virtual double cellVolume(std::size_t c) const = 0;
 };
