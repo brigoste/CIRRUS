2329a689c00eeb359b38ebe41ca7e2256ce0755d
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-11 15:10:50 -0700

    Update PhysicsModel.hpp
    make addCellSource an overriden function

--- physics/PhysicsModel.hpp
+++ physics/PhysicsModel.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "mesh/Point.hpp"
-#include "mesh/Face.hpp"
+#include "mesh/primitives/Point.hpp"
+#include "mesh/primitives/Face.hpp"
 #include "mesh/MeshBase.hpp"
 #include "discretization/FluxAccumulator.hpp"
 
@@ -10,23 +10,23 @@ class PhysicsModel
 public:
     virtual ~PhysicsModel() = default;
 
-    virtual double diffusionCoeff(const Face&, double d) const = 0;
+    virtual double diffusionCoeff(const Face&) const = 0;
     virtual double convectionCoeff(double) const = 0;
 
     // NEW: boundary closure hooks
-    virtual double wallConductivity() const = 0;
+    virtual double diffusionScalar() const = 0;
     virtual double reconstructBoundaryValue(const BoundaryPatchSystem::Condition& bc,
                                             double phiCell,
                                             double dx,
                                             bool isLeft) const = 0;
     virtual void addCellSources(const MeshBase& mesh, 
                                 std::size_t cell,
-                                FluxAccumulator& flux) const
-    {
-        // Can add source term per cell here. Only treat a single cell here as we loop in FluxBuilder.cpp 
-        (void)mesh;
-        (void)cell;
-        (void)flux;
-    }
+                                FluxAccumulator& flux) const = 0;
+    // {
+    //     // Can add source term per cell here. Only treat a single cell here as we loop in FluxBuilder.cpp 
+    //     (void)mesh;
+    //     (void)cell;
+    //     (void)flux;
+    // }
     
 };
