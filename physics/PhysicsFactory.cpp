b20eb12fd59e5aaf803e2bff8f9dea051947cf36
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-11 15:09:58 -0700

    Update PhysicsFactory.cpp -> add 'fluid'

--- physics/PhysicsFactory.cpp
+++ physics/PhysicsFactory.cpp
@@ -5,7 +5,11 @@ std::unique_ptr<PhysicsModel> PhysicsFactory::create(const PhysicsConfig& cfg)
 {
     if (cfg.type == "heat")
     {
-        return std::make_unique<HeatPhysicsModel>(cfg.k);
+        return std::make_unique<HeatPhysicsModel>(cfg.k, cfg.Sp, cfg.Su);
+    }
+    if (cfg.type == "fluid")
+    {
+        throw std::runtime_error("Fluid physics model not yet implmented.");
     }
 
     throw std::runtime_error("Unknown physics model type: " + cfg.type);
