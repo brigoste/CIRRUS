eccc06c6960740b3fed678984acf9075cc6f8635
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-19 16:17:26 -0700

    Update SinusoidalCase.hpp

--- test/verification/ManufacturedCases/SinusoidalCase.hpp
+++ test/verification/ManufacturedCases/SinusoidalCase.hpp
@@ -1,16 +1,13 @@
 #pragma once
 
 #include "tests/verification/VerificationCase.hpp"
+#include "config/SimulationConfig.hpp"
 
-class SinusoidalCase : public VerificationCase
+class Sinusoidal2D : public VerificationCase
 {
 public:
+    SimulationConfig config() const override;
 
-    double exact(
-        double x,
-        double y) const override;
-
-    double source(
-        double x,
-        double y) const override;
+    double exact(double x, double y) const override;
+    double laplacian(double x, double y) const override;
 };
