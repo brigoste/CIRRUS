bf078c2956c32c1bf8ad6300ea8d0e6cd0929c13
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-19 16:17:00 -0700

    Update QuadraticCase.hpp

--- test/verification/ManufacturedCases/QuadraticCase.hpp
+++ test/verification/ManufacturedCases/QuadraticCase.hpp
@@ -1,16 +1,13 @@
 #pragma once
 
 #include "tests/verification/VerificationCase.hpp"
+#include "config/SimulationConfig.hpp"
 
-class QuadraticCase : public VerificationCase
+class Quadratic1D : public VerificationCase
 {
 public:
+    SimulationConfig config() const override;
 
-    double exact(
-        double x,
-        double y) const override;
-
-    double source(
-        double x,
-        double y) const override;
+    double exact(double x, double y) const override;
+    double laplacian(double x, double y) const override;
 };
