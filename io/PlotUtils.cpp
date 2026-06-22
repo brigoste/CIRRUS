2e3e5132b3269fbcfd3bee3d32311e2734bff131
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-22 14:32:01 -0700

    Create PlotUtils.cpp

---
+++ io/PlotUtils.cpp
@@ -0,0 +1,25 @@
+#include "PlotUtils.hpp"
+#include <iostream>
+#include <cstdlib>
+
+constexpr const char* PYTHON_EXE =
+    "C:/Users/E40112856/.julia/conda/3/x86_64/python.exe";
+
+constexpr const char* PLOT_SCRIPT =
+    "C:/Users/E40112856/Packages/CIRRUS/scripts/Plot.py";
+
+void runPlot(const std::string& csvFile)
+{
+    std::string cmd =
+        "cmd /c "
+        "\"\"" + std::string(PYTHON_EXE) +
+        "\" \"" + PLOT_SCRIPT +
+        "\" \"" + csvFile +
+        "\"\"";
+
+    std::cout << cmd << '\n';       // forces a command prompt run of the python script
+
+    int rc = std::system(cmd.c_str());
+
+    std::cout << "\nReturn code = " << rc << "\n\n";
+}
