fd165f66d27923a2e469688b502140e5ad2544d2
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-22 20:15:56 -0700

    Update ErrorMetrics.hpp
    Add l2_energy

--- test/verification/ErrorMetrics.hpp
+++ test/verification/ErrorMetrics.hpp
@@ -8,8 +8,9 @@ class MeshBase;
 
 struct ErrorNormResults
 {
-    double l2 = 0.0;
-    double linf = 0.0;
+    double l2_energy = 0.0;
+    double l2_rms    = 0.0;
+    double linf      = 0.0;
 };
 
 class ErrorNorms
