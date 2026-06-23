9248315655b9892642698429f5fd7b0dee002f14
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-23 16:52:34 -0700

    Update PathUtils.hpp

--- config/PathUtils.hpp
+++ config/PathUtils.hpp
@@ -1,23 +1,4 @@
 #pragma once
+#include "config/PathContext.hpp"
 
-#include <filesystem>
-#include <string>
-
-class SimulationConfig;
-
-namespace Paths
-{
-    std::filesystem::path outputRoot(
-        const SimulationConfig& cfg);
-
-    std::filesystem::path verificationRoot(
-        const SimulationConfig& cfg);
-
-    std::filesystem::path verificationCSV(
-        const SimulationConfig& cfg,
-        const std::string& caseName);
-
-    std::filesystem::path verificationJSON(
-        const SimulationConfig& cfg,
-        const std::string& caseName);
-}
+PathContext buildPaths(const SimulationConfig& cfg);
