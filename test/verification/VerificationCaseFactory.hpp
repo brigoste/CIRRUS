680fc0a06fbd085e05c00304cf31ce15b15ff701
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-27 16:14:03 -0700

    Update VerificationCaseFactory.hpp

--- test/verification/VerificationCaseFactory.hpp
+++ test/verification/VerificationCaseFactory.hpp
@@ -9,5 +9,8 @@ class VerificationCaseFactory
 {
 public:
 
-    static std::unique_ptr<VerificationCase> create(const std::string& name);
+    // static std::unique_ptr<VerificationCase> create(const std::string& name);
+
+    static std::unique_ptr<VerificationCase> create(const std::string& name,
+                                                    const nlohmann::json& params);
 };
