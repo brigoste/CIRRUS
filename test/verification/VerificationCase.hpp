0c8e324e85f2a52aa254ddbccb5052c4aac13c71
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-27 16:13:23 -0700

    Update VerificationCase.hpp

--- test/verification/VerificationCase.hpp
+++ test/verification/VerificationCase.hpp
@@ -1,21 +1,18 @@
 #pragma once
 #include "Physics/HeatPhysicsModel.hpp"
 #include "config/SimulationConfig.hpp"
+#include "nlohmann/json.hpp"
 
 // Abstraction for all tests we use to verify.
-
-#pragma once
 
 class VerificationCase
 {
 public:
     virtual ~VerificationCase() = default;
 
-    virtual SimulationConfig config() const = 0;
-
-    // Manufactured solution
     virtual double exact(double x, double y) const = 0;
 
-    // RHS of PDE: -∇·(k∇φ) = S (WITHOUT k if your physics applies it)
     virtual double laplacian(double x, double y) const = 0;
+
+    virtual double source(double x, double y) const = 0;
 };
