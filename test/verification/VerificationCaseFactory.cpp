d0c26c45a50d4b5a4944bd009a9ff608488c161a
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-27 16:13:44 -0700

    Update VerificationCaseFactory.cpp
    Added AdvectionDiffusion2D case.

--- test/verification/VerificationCaseFactory.cpp
+++ test/verification/VerificationCaseFactory.cpp
@@ -3,22 +3,23 @@
 #include "ManufacturedCases/1DLinear.hpp"
 #include "ManufacturedCases/Quadratic1D.hpp"
 #include "ManufacturedCases/Sinusoidal2D.hpp"
+#include "ManufacturedCases/AdvectionDiffusion2D.hpp"
 
-std::unique_ptr<VerificationCase>
-VerificationCaseFactory::create(
-    const std::string& name)
+std::unique_ptr<VerificationCase> VerificationCaseFactory::create(
+    const std::string& name,
+    const nlohmann::json& params)
 {
-    if (name == "Linear1D"){
-        return std::make_unique<OneDLinear>();
-    }
+    if (name == "Linear1D")
+        return std::make_unique<OneDLinear>(params);
 
-    if (name == "Quadratic1D"){
-        return std::make_unique<Quadratic1D>();
-    }
+    if (name == "Quadratic1D")
+        return std::make_unique<Quadratic1D>(params);
 
-    if (name == "Sinusoidal2D"){
-        return std::make_unique<Sinusoidal2D>();
-    }
+    if (name == "Sinusoidal2D")
+        return std::make_unique<Sinusoidal2D>(params);
+
+    if (name == "AdvectionDiffusion2D")
+        return std::make_unique<AdvectionDiffusion2D>(params);
 
     throw std::runtime_error("Unknown verification case: " + name);
 }
