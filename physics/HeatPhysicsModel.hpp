21c4a1ea703fb18d028c0e4ecd50a27d00587e40
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-06-27 16:06:08 -0700

    Update HeatPhysicsModel.hpp
    Removing linearized source terms (inconsistent for general solving)

--- physics/HeatPhysicsModel.hpp
+++ physics/HeatPhysicsModel.hpp
@@ -1,29 +1,34 @@
 #pragma once
 
-#include "PhysicsModel.hpp"
+#include "physics/PhysicsModel.hpp"
 #include "mesh/primitives/Face.hpp"
-#include "utils/LinearAlgebraUtils.hpp"
 
 class HeatPhysicsModel : public PhysicsModel
 {
 public:
-    explicit HeatPhysicsModel(double k, double Su, double Sp)
-        : k_(k), Su_(Su), Sp_(Sp) {} 
+    explicit HeatPhysicsModel(double k)
+        : k_(k) {}
 
-    double diffusionScalar() const override { return k_; }
-    double diffusionCoeff(const Face& face) const;
-    double convectionCoeff(double flux) const;
-    double reconstructBoundaryValue(const BoundaryPatchSystem::Condition& bc,
-                                            double phiCell,
-                                            double dx,
-                                            bool isLeft) const override;
-    void addCellSources(const MeshBase& mesh,
-                        std::size_t c,
-                        FluxAccumulator& flux) const override;
+    double diffusionFaceCoefficient(const Face& face) const override;
+    double convectionFaceFlux(const Face& face) const override;
+
+    double diffusionCoefficient() const override { return k_; }
+
+    double reconstructBoundaryValue(
+        const BoundaryPatchSystem::Condition& bc,
+        double phiCell,
+        double dx,
+        bool isLeft) const override;
+
+    void addCellSources(
+        const MeshBase& mesh,
+        std::size_t c,
+        FluxAccumulator& flux) const override;
+
+    double cellSource(
+        const MeshBase& mesh,
+        std::size_t c) const override;
 
 private:
     double k_ = 1.0;
-    double Su_ = 0.0;
-    double Sp_ = 0.0;
-
 };
