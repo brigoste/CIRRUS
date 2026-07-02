ac27a6214fe674e5b03996fc4005397f7262b79f
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-02 16:53:12 -0700

    Update DerivedFields.hpp

--- postprocessing/DerivedFields.hpp
+++ postprocessing/DerivedFields.hpp
@@ -8,12 +8,7 @@ class DerivedFields
 {
 public:
 
-    static std::vector<double> gradient1D(
-        const MeshBase& mesh,
-        const std::vector<double>& phi);
+    static std::vector<double> gradient1D( const MeshBase& mesh, const std::vector<double>& phi);
 
-    static std::vector<double> heatFlux1D(
-        const MeshBase& mesh,
-        const std::vector<double>& phi,
-        double conductivity);
+    static std::vector<double> heatFlux1D( const MeshBase& mesh, const std::vector<double>& phi, double conductivity);
 };
