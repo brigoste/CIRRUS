ba095d7601545911727b4e8daf138b78e4861db6
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-02 16:58:59 -0700

    Update and rename SinusoidalCase.cpp to Sinusoidal2D.cpp

---
+++ test/verification/ManufacturedCases/Sinusoidal2D.cpp
@@ -0,0 +1,27 @@
+#include "tests/verification/ManufacturedCases/Sinusoidal2D.hpp"
+
+// #include "tests/verification/VerificationAutoRegister.hpp"
+// #include "tests/verification/VerificationRegistry.hpp"
+#include "utils/MathConstants.hpp"
+
+double Sinusoidal2D::exact(
+    double x,
+    double y) const
+{
+    return std::sin(math::PI * x) *
+           std::sin(math::PI * y);
+}
+
+double Sinusoidal2D::source(double x, double y) const
+{
+    return -k_ * laplacian(x, y);
+}
+
+double Sinusoidal2D::laplacian(
+    double x,
+    double y) const
+{
+    return -2.0 * math::PI * math::PI *
+           std::sin(math::PI * x) *
+           std::sin(math::PI * y);
+}
