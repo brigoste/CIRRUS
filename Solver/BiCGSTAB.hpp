2ade94505db2596f6e85b35d25b6d481eca996de
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-02 14:29:29 -0700

    Update BiCGSTAB.hpp

--- Solver/BiCGSTAB.hpp
+++ Solver/BiCGSTAB.hpp
@@ -2,7 +2,4 @@
 #include "linear_system/LinearSystem.hpp"
 #include <vector>
 
-std::vector<double> BiCGSTAB(
-    const LinearSystem& sys,
-    int max_iter,
-    double tol);
+std::vector<double> BiCGSTAB( const LinearSystem& sys, int max_iter, double tol);
