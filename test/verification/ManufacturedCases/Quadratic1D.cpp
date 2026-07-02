00b689954a78aec765b25b81c2d1d917f5767f86
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-02 16:58:20 -0700

    Update and rename QuadraticCase.cpp to Quadratic1D.cpp

---
+++ test/verification/ManufacturedCases/Quadratic1D.cpp
@@ -0,0 +1,16 @@
+#include "tests/verification/ManufacturedCases/Quadratic1D.hpp"
+
+double Quadratic1D::exact( double x, double) const
+{
+    return x;
+}
+
+double Quadratic1D::source(double x, double y) const
+{
+    return -k_ * laplacian(x, y);
+}
+
+double Quadratic1D::laplacian( double, double) const
+{
+    return -2.0;      // -2*k
+}
