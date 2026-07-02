288c5e0b6d96edf9eed80be7d7b79102c0ef5878
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-02 16:53:42 -0700

    Update ErrorNorms.hpp

--- postprocessing/ErrorNorms.hpp
+++ postprocessing/ErrorNorms.hpp
@@ -6,15 +6,9 @@ class ErrorNorms
 {
 public:
 
-    static double L1(
-        const std::vector<double>& numerical,
-        const std::vector<double>& exact);
+    static double L1( const std::vector<double>& numerical, const std::vector<double>& exact);
 
-    static double L2(
-        const std::vector<double>& numerical,
-        const std::vector<double>& exact);
+    static double L2( const std::vector<double>& numerical, const std::vector<double>& exact);
 
-    static double Linf(
-        const std::vector<double>& numerical,
-        const std::vector<double>& exact);
+    static double Linf( const std::vector<double>& numerical, const std::vector<double>& exact);
 };
