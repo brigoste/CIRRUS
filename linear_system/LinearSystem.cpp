8330b1ce9925403bba755b2c836b0499eadadb8b
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-02 14:41:14 -0700

    Update LinearSystem.cpp

--- linear_system/LinearSystem.cpp
+++ linear_system/LinearSystem.cpp
@@ -1,9 +1,6 @@
 #include "linear_system/LinearSystem.hpp"
 
-LinearSystem::LinearSystem(std::size_t n)
-{
-    resize(n);
-}
+LinearSystem::LinearSystem(std::size_t n) { resize(n); }
 
 void LinearSystem::resize(std::size_t n)
 {
@@ -12,38 +9,23 @@ void LinearSystem::resize(std::size_t n)
     b_.assign(n, 0.0);
 }
 
-std::size_t LinearSystem::size() const
-{
-    return n_;
-}
+std::size_t LinearSystem::size() const { return n_; }
 
 // ============================================================
 // MATRIX ASSEMBLY
 // ============================================================
 
-void LinearSystem::addCoeff(std::size_t i, std::size_t j, double val)
-{
-    A_[i][j] += val;   // IMPORTANT: FV accumulation semantics
-}
+// IMPORTANT: FV accumulation semantics
+void LinearSystem::addCoeff(std::size_t i, std::size_t j, double val) { A_[i][j] += val; } 
+void LinearSystem::addRHS(std::size_t i, double val) { b_[i] += val; }
 
-void LinearSystem::addRHS(std::size_t i, double val)
-{
-    b_[i] += val;
-}
-
-void LinearSystem::setRHS(std::size_t i, double val)
-{
-    b_[i] = val;
-}
+void LinearSystem::setRHS(std::size_t i, double val) { b_[i] = val; }
 
 // ============================================================
 // ACCESSORS
 // ============================================================
 
-double LinearSystem::rhs(std::size_t i) const
-{
-    return b_[i];
-}
+double LinearSystem::rhs(std::size_t i) const { return b_[i]; }
 
 double LinearSystem::coeff(std::size_t i, std::size_t j) const
 {
@@ -51,25 +33,15 @@ double LinearSystem::coeff(std::size_t i, std::size_t j) const
     return (it != A_[i].end()) ? it->second : 0.0;
 }
 
-std::vector<double>& LinearSystem::RHS()
-{
-    return b_;
-}
+std::vector<double>& LinearSystem::RHS() { return b_; }
 
-const std::vector<double>& LinearSystem::RHS() const
-{
-    return b_;
-}
+const std::vector<double>& LinearSystem::RHS() const { return b_; }
 
-const std::unordered_map<std::size_t, double>& LinearSystem::row(std::size_t i) const
-{
-    return A_[i];
-}
+const std::unordered_map<std::size_t, double>& LinearSystem::row(std::size_t i) const { return A_[i]; }
 
 void LinearSystem::clear()
 {
-    for (auto& row : A_)
-        row.clear();
-
+    for (auto& row : A_) row.clear();
+    
     std::fill(b_.begin(), b_.end(), 0.0);
 }
