3b2440dd9a3548cc654439732429b7cca58b8a7a
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-02 14:38:25 -0700

    Update FluxAccumulator.hpp

--- discretization/FluxAccumulator.hpp
+++ discretization/FluxAccumulator.hpp
@@ -44,20 +44,15 @@ public:
     // =========================================================
     void addDiffusion(std::size_t P, std::size_t N, double D)
     {
-        if (N == Face::INVALID)
-            diffusion_.push_back({P, N, D, FaceType::Boundary});
-        else
-            diffusion_.push_back({P, N, D, FaceType::Interior});
+        if (N == Face::INVALID) { diffusion_.push_back({P, N, D, FaceType::Boundary}); }
+        else { diffusion_.push_back({P, N, D, FaceType::Interior}); }
     }
     void addBoundaryDiffusion(std::size_t P, double D, double val)
     {
         cells_[P].Sp -= D;
         cells_[P].Su += D * val;   // Tw, or V, or P, whatever this is.
     }
-    void addConvection(std::size_t P, std::size_t N, double F)
-    {
-        convection_.push_back({P, N, F});
-    }
+    void addConvection(std::size_t P, std::size_t N, double F) { convection_.push_back({P, N, F}); }
 
     void addSource(std::size_t c, double Su, double Sp)
     {
@@ -68,10 +63,7 @@ public:
     // =========================================================
     // ACCESS
     // =========================================================
-    const CellResidual& operator[](std::size_t i) const
-    {
-        return cells_[i];
-    }
+    const CellResidual& operator[](std::size_t i) const { return cells_[i]; }
 
     const auto& diffusion() const { return diffusion_; }
     const auto& convection() const { return convection_; }
