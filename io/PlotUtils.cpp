30910f62157a0fcaa49cac24927f340163003041
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-02 14:40:09 -0700

    Update PlotUtils.cpp

--- io/PlotUtils.cpp
+++ io/PlotUtils.cpp
@@ -2,11 +2,9 @@
 #include <iostream>
 #include <cstdlib>
 
-constexpr const char* PYTHON_EXE =
-    "C:/Users/E40112856/.julia/conda/3/x86_64/python.exe";
+constexpr const char* PYTHON_EXE = "C:/Users/E40112856/.julia/conda/3/x86_64/python.exe";
 
-constexpr const char* PLOT_SCRIPT =
-    "C:/Users/E40112856/Packages/CIRRUS/scripts/Plot.py";
+constexpr const char* PLOT_SCRIPT = "C:/Users/E40112856/Packages/CIRRUS/scripts/Plot.py";
 
 void runPlot(const std::string& csvFile)
 {
