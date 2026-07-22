763665b9a456c53550063bfa8d29706d511d501a
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-22 10:00:49 -0700

    Update 1DLinear.hpp

--- test/verification/ManufacturedCases/1DLinear.hpp
+++ test/verification/ManufacturedCases/1DLinear.hpp
@@ -9,10 +9,15 @@ public:
     {
         (void) params;
     }
+    void initialize(const MeshBase&) override {}  // Default: nothing needed 
+    
 
     double exact( double x, double y) const override;
 
     double laplacian( double x, double y) const override;
 
     double source(double , double ) const override { return 0.0; }
+
+    double l2AcceptanceThreshold() const override { return 1e-10; }
+    double linfAcceptanceThreshold() const override { return 1e-10; }
 };
