5a56f7df0e1cfb402e4d51d0bcdcb68a4b4413f5
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-28 11:20:43 -0700

    Update 1DLinear.cpp
    Remove commented included

--- test/verification/ManufacturedCases/1DLinear.cpp
+++ test/verification/ManufacturedCases/1DLinear.cpp
@@ -1,6 +1,5 @@
 #include "tests/verification/ManufacturedCases/1DLinear.hpp"
-// #include "tests/verification/VerificationAutoRegister.hpp"
-// #include "tests/verification/VerificationRegistry.hpp"
+
 double OneDLinear::exact(double x, double) const
 {
     return k_ * (3 + (1 / 1.5) * x);
