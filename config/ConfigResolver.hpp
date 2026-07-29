b11dca828a2194d705fbdab22c2a26558e7a2b13
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-28 21:20:30 -0700

    Update ConfigResolver.hpp

--- config/ConfigResolver.hpp
+++ config/ConfigResolver.hpp
@@ -6,6 +6,8 @@
 #include <fstream>
 #include "config/SimulationConfig.hpp"
 
+// struct SimulationConfig;
+
 class ConfigResolver
 {
 public:
@@ -14,7 +16,9 @@ public:
     SimulationConfig load(const std::filesystem::path& path);
 
 private:
-    nlohmann::json loadAndResolve(const std::filesystem::path& path);
-    nlohmann::json resolveFile(const std::filesystem::path& path);
+    nlohmann::json loadAndResolve( const std::filesystem::path& path);
+
+    nlohmann::json resolveFile( const std::filesystem::path& path);
+
     std::unordered_map<std::filesystem::path, nlohmann::json> cache;
-}
+};
