3b2cc9c3d2fd3a4b9cf0e411ee454b1aad54acc8
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-30 12:10:55 -0700

    Update PlotUtils.cpp

--- io/PlotUtils.cpp
+++ io/PlotUtils.cpp
@@ -6,19 +6,17 @@
 
 void runPlot(
     const PathContext& paths,
-    const std::filesystem::path& csvFile
-)
+    const std::filesystem::path& csvFile)
 {
-    const std::filesystem::path pythonExecutable = "C:/Users/E40112856/.julia/conda/3/x86_64/python.exe";
-
     const auto plotScript = paths.scriptRoot / "Plot.py";
 
     std::string cmd =
         "cmd /c "
-        "\"\"" + pythonExecutable.string() +
-        "\" \"" + plotScript.string() +
-        "\" \"" + csvFile.string() +
-        "\"\"";
+        + paths.pythonExecutable.string()
+        + " "
+        + plotScript.string()
+        + " "
+        + csvFile.string();
 
     std::cout << cmd << '\n';
 
