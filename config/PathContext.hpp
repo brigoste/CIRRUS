a0c5864050c82aa10c9f3bb425924d1ed9d14dd8
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-30 10:53:18 -0700

    Update PathContext.hpp

--- config/PathContext.hpp
+++ config/PathContext.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
 #include <filesystem>
-#include <string>
-
-#include "config/SimulationConfig.hpp"
 
 struct PathContext
 {
+    std::filesystem::path projectRoot;
+    
     std::filesystem::path outputRoot;
     std::filesystem::path verificationRoot;
+    std::filesystem::path scriptRoot;
 };
