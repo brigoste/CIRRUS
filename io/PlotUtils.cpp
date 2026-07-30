9e1ab98ba5c7d758f803790888d5741e26ba5acf
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-07-30 10:58:17 -0700

    Update PlotUtils.cpp

--- io/PlotUtils.cpp
+++ io/PlotUtils.cpp
@@ -1,23 +1,30 @@
 #include "PlotUtils.hpp"
+
 #include <iostream>
 #include <cstdlib>
 
-constexpr const char* PYTHON_EXE = "C:/Users/E40112856/.julia/conda/3/x86_64/python.exe";
 
-constexpr const char* PLOT_SCRIPT = "C:/Users/E40112856/Packages/CIRRUS/scripts/Plot.py";
-
-void runPlot(const std::string& csvFile)
+void runPlot(
+    const PathContext& paths,
+    const std::filesystem::path& csvFile
+)
 {
+    const std::filesystem::path pythonExecutable = "C:/Users/E40112856/.julia/conda/3/x86_64/python.exe";
+
+    const auto plotScript = paths.scriptRoot / "Plot.py";
+
     std::string cmd =
         "cmd /c "
-        "\"\"" + std::string(PYTHON_EXE) +
-        "\" \"" + PLOT_SCRIPT +
-        "\" \"" + csvFile +
+        "\"\"" + pythonExecutable.string() +
+        "\" \"" + plotScript.string() +
+        "\" \"" + csvFile.string() +
         "\"\"";
 
-    std::cout << cmd << '\n';       // forces a command prompt run of the python script
+    std::cout << cmd << '\n';
 
     int rc = std::system(cmd.c_str());
 
-    std::cout << "\nReturn code = " << rc << "\n\n";
+    std::cout << "\nReturn code = "
+              << rc
+              << "\n\n";
 }
