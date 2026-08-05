ac84d7a1fc8e2750fca9f9a88f19c190a313095f
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-04 20:56:32 -0700

    Update BiCGSTAB.hpp

--- solver/BiCGSTAB.hpp
+++ solver/BiCGSTAB.hpp
@@ -1,5 +1,10 @@
 #pragma once
 #include "linear_system/LinearSystem.hpp"
+#include "solver/preconditioners/Preconditioner.hpp"
 #include <vector>
 
-std::vector<double> BiCGSTAB( const LinearSystem& sys, int max_iter, double tol);
+std::vector<double> BiCGSTAB(
+    const LinearSystem& sys,
+    int max_iter,
+    double tol,
+    const Preconditioner& M);
