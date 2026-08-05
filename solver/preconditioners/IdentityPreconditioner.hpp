f7ee7257527779f8d4451fbec3722a70f73f7654
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-04 20:52:21 -0700

    Create IdentityPreconditioner.hpp

---
+++ solver/preconditioners/IdentityPreconditioner.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "solver/preconditioners/Preconditioner.hpp"
+
+class IdentityPreconditioner : public Preconditioner
+{
+public:
+
+    void setup(const LinearSystem& sys) override;
+    void apply( const std::vector<double>& r, std::vector<double>& z) const override;
+    std::string name() const override { return "None"; }
+};
