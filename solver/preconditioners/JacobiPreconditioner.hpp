196ed0d08450dc4fef9b2fd27efc85f705fab5b2
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-04 20:54:16 -0700

    Create JacobiPreconditioner.hpp

---
+++ solver/preconditioners/JacobiPreconditioner.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "solver/preconditioners/Preconditioner.hpp"
+#include "linear_system/LinearSystem.hpp"
+
+#include <vector>
+
+
+class JacobiPreconditioner : public Preconditioner
+{
+public:
+
+    void setup(const LinearSystem& sys) override;
+
+
+    void apply(
+        const std::vector<double>& r,
+        std::vector<double>& z) const override;
+
+        std::string name() const override { return "Jacobi"; }
+
+private:
+
+    std::vector<double> diagInv_;
+};
