c106ff87cdabbfe20f4b07c2e7035c31d1765cf2
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-05 10:20:38 -0700

    Create Timer.hpp

---
+++ utils/Timer.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <chrono>
+#include <string>
+#include <iostream>
+
+class Timer
+{
+public:
+
+    Timer(const std::string& name)
+        :
+        name_(name),
+        start_(std::chrono::high_resolution_clock::now())
+    {
+    }
+
+    ~Timer()
+    {
+        stop();
+    }
+
+    void stop()
+    {
+        if (!running_)
+            return;
+
+        auto end = std::chrono::high_resolution_clock::now();
+
+        double elapsed =
+            std::chrono::duration<double>(end - start_).count();
+
+        std::cout
+            << "[TIMER] "
+            << name_
+            << ": "
+            << elapsed
+            << " s\n";
+
+        running_ = false;
+    }
+
+private:
+
+    std::string name_;
+    std::chrono::high_resolution_clock::time_point start_;
+    bool running_ = true;
+};
