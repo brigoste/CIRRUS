1a6006c7bb38e3e48f92044d5109238daf799ba6
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-04 20:54:42 -0700

    Create Preconditioner.hpp

---
+++ solver/preconditioners/Preconditioner.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <string>
+#include <stdexcept>
+#include <vector>
+#include <nlohmann/json.hpp>
+#include "solver/preconditioners/PreconditionerType.hpp"
+
+class LinearSystem;
+
+inline PreconditionerType preconditioner_from_string(
+    const std::string& s)
+{
+    if (s == "None")   return PreconditionerType::None;
+    if (s == "Jacobi") return PreconditionerType::Jacobi;
+    if (s == "ILU0")   return PreconditionerType::ILU0;
+
+    throw std::runtime_error(
+        "Unknown preconditioner: " + s);
+}
+
+
+inline void from_json(
+    const nlohmann::json& j,
+    PreconditionerType& p)
+{
+    p = preconditioner_from_string( j.get<std::string>());
+}
+
+
+inline void to_json(
+    nlohmann::json& j,
+    const PreconditionerType& p)
+{
+    switch (p)
+    {
+        case PreconditionerType::None:
+            j = "None";
+            break;
+
+        case PreconditionerType::Jacobi:
+            j = "Jacobi";
+            break;
+
+        case PreconditionerType::ILU0:
+            j = "ILU0";
+            break;
+    }
+}
+
+class Preconditioner
+{
+public:
+
+    virtual ~Preconditioner() = default;
+
+    virtual void setup(
+        const LinearSystem& sys) = 0;
+
+    virtual void apply(
+        const std::vector<double>& r,
+        std::vector<double>& z) const = 0;
+
+    virtual std::string name() const = 0;
+};
