daef4833e447dabf9fe0216dcba345c6beb01d3a
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-05 10:13:53 -0700

    Create ScalarField.cpp

---
+++ fields/ScalarField.cpp
@@ -0,0 +1,46 @@
+#include "fields/ScalarField.hpp"
+
+#include <stdexcept>
+#include <algorithm>
+#include <utility>
+
+ScalarField::ScalarField(
+    std::string name,
+    const MeshBase& mesh,
+    FieldLocation location,
+    double initialValue
+) 
+: 
+FieldBase(std::move(name), mesh, location)
+{
+    switch (location_)
+    {
+        case FieldLocation::Cell:
+            values_.assign(mesh_.ncells(), initialValue);
+            break;
+
+        case FieldLocation::Face:
+            values_.assign(mesh_.nfaces(), initialValue);
+            break;
+
+        case FieldLocation::Node:
+            values_.assign(mesh_.nnodes(), initialValue);
+            break;
+
+        default:
+            throw std::runtime_error("Unknown field location.");
+    }
+}
+
+std::size_t ScalarField::size() const { return values_.size(); }
+double& ScalarField::operator[](std::size_t i) { return values_[i]; }
+const double& ScalarField::operator[](std::size_t i) const { return values_[i]; }
+
+void ScalarField::fill(double value) 
+{ 
+    std::fill(
+        values_.begin(), 
+        values_.end(), 
+        value
+    ); 
+}
