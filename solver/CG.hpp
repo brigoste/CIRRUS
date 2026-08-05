75a68b732d221d681d26a9d7bda253e46a8f46c7
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-04 20:56:57 -0700

    Update CG.hpp

--- solver/CG.hpp
+++ solver/CG.hpp
@@ -1,5 +1,13 @@
 #pragma once
+
 #include "linear_system/LinearSystem.hpp"
+#include "solver/preconditioners/Preconditioner.hpp"
+
 #include <vector>
 
-std::vector<double> CG( const LinearSystem& sys, int max_iter, double tol);
+
+std::vector<double> CG(
+    const LinearSystem& sys,
+    int max_iter,
+    double tol,
+    const Preconditioner& M);
