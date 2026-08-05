1247c22e3d56f1f935a419e8b5e8a065e1ed3d71
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-05 10:14:05 -0700

    Create ScalarField.hpp

---
+++ fields/ScalarField.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vector>
+#include <cstddef>
+
+#include "fields/FieldBase.hpp"
+
+class ScalarField : public FieldBase
+{
+public:
+
+    ScalarField(
+        std::string name,
+        const MeshBase& mesh,
+        FieldLocation location,
+        double initialValue = 0.0
+    );
+
+    // value access
+    double& operator[](std::size_t i);
+    const double& operator[](std::size_t i) const;
+
+    // FieldBase interface
+    std::size_t size() const override;
+
+    // Utility
+    void fill(double value);
+
+private:
+
+    std::vector<double> values_;
+};
