2d0f018998149c2a815e2f9ad0479b9ccffad829
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-06 12:17:16 -0700

    Update 1DLinear.hpp

--- test/verification/ManufacturedCases/1DLinear.hpp
+++ test/verification/ManufacturedCases/1DLinear.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
-#include "tests/verification/VerificationCase.hpp"
+#include "test/verification/VerificationCase.hpp"
 
 /*
     Documentation:
 
     Case                    Solver      Mesh        L2 Error      L2 Check    Refinement    Order
     --------------------------------------------------------------------------------------------------
-    Linear1D                TDMA        10x1        2.948e-13     PASS        N/A           N/A
+    Linear1D                TDMA        20x1        2.049e-13     PASS        N/A           N/A
 */
 
 class OneDLinear : public VerificationCase
@@ -22,10 +22,10 @@ public:
 
     double laplacian( double x, double y) const override;
 
-    double source(double , double ) const override { return 0.0; }
+    double source(double , double ) const override                  { return 0.0; }
 
-    double l2AcceptanceThreshold() const override { return 1e-10; }
-    double linfAcceptanceThreshold() const override { return 1e-10; }
+    double l2AcceptanceThreshold() const override                   { return 1e-10; }
+    double linfAcceptanceThreshold() const override                 { return 1e-10; }
 
 private:
     double k_;
