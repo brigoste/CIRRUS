aa0281aec6f37d734804123296c87d57046ab0a1
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-06 12:02:32 -0700

    Update ILU0Preconditioner.hpp

--- solver/preconditioners/ILU0Preconditioner.hpp
+++ solver/preconditioners/ILU0Preconditioner.hpp
@@ -2,25 +2,11 @@
 
 #include "solver/preconditioners/Preconditioner.hpp"
 
-#include <vector>
-#include <unordered_map>
-
-class ILU0Preconditioner : public Preconditioner
+class IdentityPreconditioner : public Preconditioner
 {
 public:
 
-    void copyMatrix(const LinearSystem& sys);
-    void setup(const LinearSystem& sys);
-    void factorize();
-
+    void setup(const LinearSystem& sys) override;
     void apply( const std::vector<double>& r, std::vector<double>& z) const override;
-
-    std::string name() const override { return "ILU0"; }
-
-private:
-
-    // L and U storage
-    std::vector<std::unordered_map<std::size_t,double>> LU_;
-
-    std::size_t N_ = 0;
+    std::string name() const override { return "None"; }
 };
