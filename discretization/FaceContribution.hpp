cff619fbb7dbeffa9f9ef0fab235461b6d429edb
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-11 16:10:47 -0700

    Update FaceContribution.hpp

--- discretization/FaceContribution.hpp
+++ discretization/FaceContribution.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
+#include "mesh/primitives/Face.hpp"
 #include <cstddef>
 
 struct FaceContribution
 {
     std::size_t P = 0;
-    std::size_t N = 0;
+    std::size_t N = Face::INVALID;
     std::size_t face = 0;
 };
