c2ff74320d7ec5695d906c9ee0e0daaeb295a86e
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-11 10:44:14 -0700

    Update Point.hpp

--- mesh/primitives/Point.hpp
+++ mesh/primitives/Point.hpp
@@ -1,45 +1,48 @@
 #pragma once
+
 #include <array>
+#include "mesh/primitives/Vector.hpp"
 
 struct Point
 {
-    std::array<double, 3> x{};  // fixed max dimension
+    std::array<double, 3> x{};
 
     double& operator[](std::size_t i) { return x[i]; }
-    double  operator[](std::size_t i) const { return x[i]; }
+    double operator[](std::size_t i) const { return x[i]; }
 
     std::size_t dim() const { return 3; }
 };
 
-inline Point operator+(const Point& a, const Point& b)
+// Point - Point = Vector
+inline Vector operator-(const Point& a, const Point& b)
+{
+    return Vector{
+        a.x[0] - b.x[0],
+        a.x[1] - b.x[1],
+        a.x[2] - b.x[2]
+    };
+}
+
+// Point + Vector = Point
+inline Point operator+(const Point& p, const Vector& v)
 {
     return Point{
         {
-            a.x[0] + b.x[0],
-            a.x[1] + b.x[1],
-            a.x[2] + b.x[2]
+            p.x[0] + v.x,
+            p.x[1] + v.y,
+            p.x[2] + v.z
         }
     };
 }
 
-inline Point operator-(const Point& a, const Point& b)
+// Point - Vector = Point
+inline Point operator-(const Point& p, const Vector& v)
 {
     return Point{
         {
-            a.x[0] - b.x[0],
-            a.x[1] - b.x[1],
-            a.x[2] - b.x[2]
-        }
-    };
-}
-
-inline Point operator*(double s, const Point& a)
-{
-    return Point{
-        {
-            s * a.x[0],
-            s * a.x[1],
-            s * a.x[2]
+            p.x[0] - v.x,
+            p.x[1] - v.y,
+            p.x[2] - v.z
         }
     };
 }
