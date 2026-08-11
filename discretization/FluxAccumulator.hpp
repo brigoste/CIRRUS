9be7871c52edd6cea24779e88c7ebbf8639f0c63
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-11 14:36:20 -0700

    Update FluxAccumulator.hpp

--- discretization/FluxAccumulator.hpp
+++ discretization/FluxAccumulator.hpp
@@ -6,6 +6,7 @@
 #include "discretization/CellResidual.hpp"
 #include "discretization/FaceDiffusion.hpp"
 #include "discretization/FaceConvection.hpp"
+#include "discretization/MatrixContribution.hpp"
 #include "mesh/BoundaryPatchSystem.hpp"
 #include "mesh/primitives/Face.hpp"
 
@@ -37,29 +38,26 @@ public:
 
         diffusion_.clear();
         convection_.clear();
+        matrixContributions_.clear();
     }
 
     // =========================================================
     // ADDERS
     // =========================================================
-    void addDiffusion(std::size_t P, std::size_t N, double D)
-    {
-        diffusion_.push_back({P, N, D});
-    }
+    void addDiffusion(std::size_t P, std::size_t N, double D) { diffusion_.push_back({P, N, D}); }
     void addBoundaryDiffusion(std::size_t P, double D, double value)
     {
         cells_[P].Sp -= D;
         cells_[P].Su += D * value;  
     }
-    void addConvection(std::size_t P, std::size_t N, std::size_t face, double F) 
-    { 
-        convection_.push_back({P, N, face, F}); 
-    }
+    void addConvection(std::size_t P, std::size_t N, std::size_t face, double F) { convection_.push_back({P, N, face, F});  }
     void addSource(std::size_t c, double Su, double Sp)
     {
         cells_[c].Su += Su;
         cells_[c].Sp += Sp;
     }
+
+    void addMatrixContribution( std::size_t row, std::size_t column, double coefficient) { matrixContributions_.push_back( {row, column, coefficient} ); }
 
     // =========================================================
     // ACCESS
@@ -68,6 +66,7 @@ public:
 
     const auto& diffusion() const { return diffusion_; }
     const auto& convection() const { return convection_; }
+    const auto& matrixContributions() const { return matrixContributions_; }
 
     std::size_t size() const { return cells_.size(); }
 
@@ -75,4 +74,5 @@ private:
     std::vector<CellResidual> cells_;
     std::vector<FaceDiffusion> diffusion_ = {};
     std::vector<FaceConvection> convection_;
+    std::vector<MatrixContribution> matrixContributions_;
 };
