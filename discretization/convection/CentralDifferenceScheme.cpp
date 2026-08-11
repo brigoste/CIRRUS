5e21adbe801cdc3d4fa7496087c540c99f7fc355
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-11 10:38:01 -0700

    Update CentralDifferenceScheme.cpp

--- discretization/convection/CentralDifferenceScheme.cpp
+++ discretization/convection/CentralDifferenceScheme.cpp
@@ -1,40 +1,8 @@
 #include "discretization/convection/CentralDifferenceScheme.hpp"
 
-#include "mesh/MeshBase.hpp"
-#include "mesh/primitives/Face.hpp"
-
-#include "utils/LinearAlgebraUtils.hpp"
-
-#include <cmath>
-#include <stdexcept>
-
-ConvectionCoefficients CentralDifferenceScheme::coefficients(
-    const MeshBase& mesh,
-    const FaceConvection& face,
-    const ScalarField& /*field*/,
-    const VectorField& /*gradient*/
+double CentralDifferenceScheme::faceCoefficient(
+    const FaceConvection& face
 ) const
 {
-    const Face& f = mesh.face(face.face);
-    const Point& xP = mesh.cellCenter(face.P);
-
-    const double dPN =
-        std::abs(LA::dot(f.dPN, f.normal));
-
-    if (dPN <= 0.0)
-    {
-        throw std::runtime_error(
-            "CentralDifferenceScheme: invalid face spacing."
-        );
-    }
-
-    const double dPF =
-        std::abs(LA::dot(f.center - xP, f.normal));
-
-    const double alpha = dPF / dPN;
-
-    return {
-        face.F * (1.0 - alpha),
-        face.F * alpha
-    };
+    return face.F;
 }
