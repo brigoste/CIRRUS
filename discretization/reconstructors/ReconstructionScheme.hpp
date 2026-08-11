bff5415e7a2d6b223ab9981b1ab623209d87c33f
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-11 10:42:32 -0700

    Update ReconstructionScheme.hpp

--- discretization/reconstructors/ReconstructionScheme.hpp
+++ discretization/reconstructors/ReconstructionScheme.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <cstddef>
+#include <stdexcept>
+
+#include "discretization/reconstructors/ReconstructionStencil.hpp"
 
 class MeshBase;
 class ScalarField;
@@ -13,11 +16,22 @@ public:
 
     virtual ~ReconstructionScheme() = default;
 
-    virtual double reconstruct(
+    virtual double reconstruct(                 //gives you a linear representation of that reconstruction:
         const MeshBase& mesh,
         std::size_t owner,
         const Face& face,
         const ScalarField& field,
         const VectorField& gradient
     ) const = 0;
+    
+    virtual ReconstructionStencil stencil(      //gives you the actual face value:
+        const MeshBase& /*mesh*/,
+        std::size_t /*owner*/,
+        const Face& /*face*/,
+        const ScalarField& /*field*/,
+        const VectorField& /*gradient*/
+    ) const 
+    {
+        throw std::runtime_error( "Reconstruction scheme does not provide a cell-value stencil." );
+    }
 };
