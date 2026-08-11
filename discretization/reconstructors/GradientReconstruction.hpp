d535fb4f1729fe9770410034882d28575d0ab3db
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-11 10:41:19 -0700

    Update GradientReconstruction.hpp

--- discretization/reconstructors/GradientReconstruction.hpp
+++ discretization/reconstructors/GradientReconstruction.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
-#include <cstddef>
-
 #include "discretization/reconstructors/ReconstructionScheme.hpp"
 
 class GradientReconstruction : public ReconstructionScheme
 {
 public:
+
+    // ReconstructionStencil stencil(
+    //     const MeshBase& mesh,
+    //     std::size_t owner,
+    //     const Face& face,
+    //     const ScalarField& field,
+    //     const VectorField& gradient
+    // ) const override;
 
     double reconstruct(
         const MeshBase& mesh,
