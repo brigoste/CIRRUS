443655178fb8faae8c58d70d1f7d072c566b92b7
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-11 10:41:08 -0700

    Update GradientReconstruction.cpp

--- discretization/reconstructors/GradientReconstruction.cpp
+++ discretization/reconstructors/GradientReconstruction.cpp
@@ -17,7 +17,18 @@ double GradientReconstruction::reconstruct(
     const Point& xP = mesh.cellCenter(owner);
     const Point& xF = face.center;
 
-    const Point displacement = xF - xP;
+    const Vector displacement = xF - xP;
 
     return field[owner] + LA::dot(displacement, gradient[owner]);
 }
+
+// ReconstructionStencil GradientReconstruction::stencil(
+//         const MeshBase& mesh,
+//         std::size_t owner,
+//         const Face& face,
+//         const ScalarField& field,
+//         const VectorField& gradient
+//     ) const
+// {
+//     return;
+// }
