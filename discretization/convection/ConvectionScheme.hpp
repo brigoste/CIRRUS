28214fad8c59c0b7891fb011eedefb09b4188c54
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-11 10:38:14 -0700

    Update ConvectionScheme.hpp

--- discretization/convection/ConvectionScheme.hpp
+++ discretization/convection/ConvectionScheme.hpp
@@ -2,25 +2,10 @@
 
 #include "discretization/FaceConvection.hpp"
 
-class MeshBase;
-class ScalarField;
-class VectorField;
-
-struct ConvectionCoefficients
-{
-    double owner = 0.0;
-    double neighbor = 0.0;
-};
-
 class ConvectionScheme
 {
 public:
     virtual ~ConvectionScheme() = default;
 
-    virtual ConvectionCoefficients coefficients(
-        const MeshBase& mesh,
-        const FaceConvection& face,
-        const ScalarField& field,
-        const VectorField& gradient
-    ) const = 0;
+    virtual double faceCoefficient( const FaceConvection& face ) const = 0;
 };
