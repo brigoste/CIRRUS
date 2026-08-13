37f4e2ca404701b1dbcc91b6eca9de7015cb3ebd
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-13 10:17:28 -0700

    Update SecondOrderUpwindReconstruction.hpp

--- discretization/reconstructors/SecondOrderUpwindReconstruction.hpp
+++ discretization/reconstructors/SecondOrderUpwindReconstruction.hpp
@@ -6,10 +6,19 @@ class SecondOrderUpwindReconstruction : public ReconstructionScheme
 {
 public:
 
+    ReconstructionStencil stencil(
+        const MeshBase& mesh,
+        std::size_t owner,
+        std::size_t f,
+        const ScalarField& field,
+        const VectorField& gradient,
+        double flux
+    ) const override;
+
     double reconstruct(
         const MeshBase& mesh,
         std::size_t owner,
-        const Face& face,
+        std::size_t f,
         const ScalarField& field,
         const VectorField& gradient,
         double flux
