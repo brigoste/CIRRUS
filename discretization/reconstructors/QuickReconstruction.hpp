2f43a551a29b00ce0b51c4302d68f41b94afd52e
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-13 15:58:20 -0700

    Create QuickReconstruction.hpp

---
+++ discretization/reconstructors/QuickReconstruction.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "discretization/reconstructors/ReconstructionScheme.hpp"
+
+class QuickReconstruction : public ReconstructionScheme
+{
+public:
+
+    ReconstructionStencil stencil(
+        const MeshBase& mesh,
+        std::size_t owner,
+        std::size_t f,
+        const ScalarField& field,
+        const VectorField& gradient,
+        double flux
+    ) const override;
+};
