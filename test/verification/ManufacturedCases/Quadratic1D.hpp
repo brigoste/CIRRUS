8bfa91433408237b7aa1de1824a3adc1351e3ae8
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-14 11:18:15 -0700

    Update Quadratic1D.hpp

--- test/verification/ManufacturedCases/Quadratic1D.hpp
+++ test/verification/ManufacturedCases/Quadratic1D.hpp
@@ -20,7 +20,7 @@ class Quadratic1D : public VerificationCase
 public:
     explicit Quadratic1D(const SimulationConfig& cfg)
     {
-        k_ = cfg.physics.k;
+        k_ = cfg.physics.transferCoefficient;
         volumetricSource_ = cfg.physics.volumetricSource;
         bool foundLeft = false;
         bool foundRight = false;
