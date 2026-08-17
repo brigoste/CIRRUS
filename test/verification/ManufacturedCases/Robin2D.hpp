8edd2d55a11f8576d76a84093715030959b8af7c
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-17 09:59:29 -0700

    Update Robin2D.hpp - consume Point

--- test/verification/ManufacturedCases/Robin2D.hpp
+++ test/verification/ManufacturedCases/Robin2D.hpp
@@ -25,11 +25,11 @@ public:
 
     void initialize(const MeshBase&) override {}
 
-    double exact(double x, double y) const override;
+    double exact(Point p) const override;
 
-    double laplacian(double x, double y) const override;
+    double laplacian(Point p) const override;
 
-    double source(double x, double y) const override;
+    double source(Point p) const override;
 
     double manufacturedBoundaryFlux(const Face& face) const override;
 
