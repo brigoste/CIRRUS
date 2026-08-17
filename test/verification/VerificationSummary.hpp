b23be116f15ee484fdd58ea39f009a5eb0738667
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-17 12:32:18 -0700

    Update VerificationSummary.hpp

--- test/verification/VerificationSummary.hpp
+++ test/verification/VerificationSummary.hpp
@@ -23,9 +23,27 @@ struct VerificationSummary
 
     bool refinementEnabled = false;
     bool refinementPassed = false;
+
+    double refinementRatio = 0.0;
+    double safetyFactor = 1.25;
+
     double l2Order = 0.0;
     double linfOrder = 0.0;
     double qoiOrder = 0.0;
+
+    // Richardson extrapolation
+    double l2Richardson = 0.0;
+    double linfRichardson = 0.0;
+    double qoiRichardson = 0.0;
+
+    // Grid Convergence Index
+    double l2AbsoluteGCI = 0.0;
+    double linfAbsoluteGCI = 0.0;
+    double qoiAbsoluteGCI = 0.0;
+
+    double l2RelativeGCI = 0.0;
+    double linfRelativeGCI = 0.0;
+    double qoiRelativeGCI = 0.0;
 
     // Overall case status
     bool passed() const
