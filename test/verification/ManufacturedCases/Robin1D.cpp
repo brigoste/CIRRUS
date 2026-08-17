7d9a45aaa3c6c6b04e6883bca7aec6603cb4d20d
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-17 09:58:44 -0700

    Update Robin1D.cpp - consume Point

--- test/verification/ManufacturedCases/Robin1D.cpp
+++ test/verification/ManufacturedCases/Robin1D.cpp
@@ -1,17 +1,17 @@
 #include "test/verification/ManufacturedCases/Robin1D.hpp"
 #include <iostream>
 
-double Robin1D::exact(double x, double) const
+double Robin1D::exact(Point p) const
 {
-    return 1.0 + x + x*x;
+    return 1.0 + p.x[0] + (p.x[0]*p.x[0]);
 }
 
-double Robin1D::source(double x, double) const
+double Robin1D::source(Point p) const
 {
-    return -k_ * laplacian(x, 0.0);
+    return -k_ * laplacian(p);
 }
 
-double Robin1D::laplacian(double, double) const
+double Robin1D::laplacian(Point) const
 {
     return 2.0;
 }
@@ -21,9 +21,11 @@ RobinData Robin1D::manufacturedRobinBoundary(const Face& face) const
     // Created for a right-boundary robin condition. For left, change dTdx in T_inf to dTdn.
     RobinData robin{};
 
-    const double x = face.center.x[0];
+    double x = face.center.x[0];
+    Point p;
+    p.x[0] = x;
 
-    const double T    = exact(x, 0.0);
+    const double T    = exact(p);
     const double dTdx = 1.0 + 2.0*x;
 
     constexpr double h = 10.0;
