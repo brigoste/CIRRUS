bd32f39716570154eb2f8f07348977363a7410f4
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-17 09:57:17 -0700

    Update Neumann1D.cpp - consume Point

--- test/verification/ManufacturedCases/Neumann1D.cpp
+++ test/verification/ManufacturedCases/Neumann1D.cpp
@@ -1,18 +1,18 @@
 #include "test/verification/ManufacturedCases/Neumann1D.hpp"
 
-double Neumann1D::exact(double x, double) const
+double Neumann1D::exact(Point p) const
 {
     return TL_
-        + ((-qR_ + volumetricSource_ * L_) / k_) * x
-        - (volumetricSource_ / (2.0 * k_)) * x * x;
+        + ((-qR_ + volumetricSource_ * L_) / k_) * p.x[0]
+        - (volumetricSource_ / (2.0 * k_)) * p.x[0]*p.x[0];
 }
 
-double Neumann1D::source(double, double) const
+double Neumann1D::source(Point) const
 {
     return volumetricSource_;
 }
 
-double Neumann1D::laplacian(double, double) const
+double Neumann1D::laplacian(Point) const
 {
     return -volumetricSource_ / k_;
 }
