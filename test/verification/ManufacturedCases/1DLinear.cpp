46e20291e72da3d8fef9f8f81e368eadfe2e1b6b
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-17 09:56:14 -0700

    Update 1DLinear.cpp - consume Point

--- test/verification/ManufacturedCases/1DLinear.cpp
+++ test/verification/ManufacturedCases/1DLinear.cpp
@@ -1,11 +1,11 @@
 #include "test/verification/ManufacturedCases/1DLinear.hpp"
 
-double OneDLinear::exact(double x, double) const
+double OneDLinear::exact(Point p) const
 {
-    return 100.0 + 100.0 * x;
+    return 100.0 + 100.0 * p.x[0];
 }
 
-double OneDLinear::laplacian(double,double) const
+double OneDLinear::laplacian( Point ) const
 {
     return 0.0;
 }
