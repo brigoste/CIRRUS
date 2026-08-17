0a1da6c8e7378b713632707bfb146ba3fca77b37
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-17 09:59:10 -0700

    Update Robin2D.cpp - consume Point

--- test/verification/ManufacturedCases/Robin2D.cpp
+++ test/verification/ManufacturedCases/Robin2D.cpp
@@ -1,24 +1,25 @@
 #include "test/verification/ManufacturedCases/Robin2D.hpp"
 #include "utils/MathConstants.hpp"
+#include "mesh/primitives/Point.hpp"
 
-double Robin2D::exact(double x, double y) const
+double Robin2D::exact(Point p) const
 {
     return T0_
-         + a_*x
-         + c_*x*x
-         + d_*std::cos(math::PI*y/Ly_);
+         + a_*p.x[0]
+         + c_*(p.x[0]*p.x[0])
+         + d_*std::cos(math::PI*p.x[1]/Ly_);
 }
 
-double Robin2D::laplacian(double /*x*/, double y) const
+double Robin2D::laplacian(Point p) const
 {
     return 2.0*c_
          - d_*std::pow(math::PI/Ly_,2)
-           *std::cos(math::PI*y/Ly_);
+           *std::cos(math::PI*p.x[1]/Ly_);
 }
 
-double Robin2D::source(double x, double y) const
+double Robin2D::source(Point p) const
 {
-    return -k_ * laplacian(x,y);
+    return -k_ * laplacian(p);
 }
 
 double Robin2D::manufacturedBoundaryFlux(const Face& face) const
@@ -43,8 +44,11 @@ RobinData Robin2D::manufacturedRobinBoundary(const Face& face) const
 
     const double x = face.center.x[0];
     const double y = face.center.x[1];
+    Point p;
+    p.x[0] = x;
+    p.x[1] = y;
 
-    const double T = exact(x,y);
+    const double T = exact(p);
 
     const double dTdx = a_ + 2.0*c_*x;
 
