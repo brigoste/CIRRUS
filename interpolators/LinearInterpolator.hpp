1cd2ba9c3e85346f24243b78818863de02196392
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-19 09:12:19 -0700

    Update LinearInterpolator.hpp

--- interpolators/LinearInterpolator.hpp
+++ interpolators/LinearInterpolator.hpp
@@ -1,13 +1,43 @@
 #pragma once
 
 #include "interpolators/Interpolator.hpp"
+
 #include "fields/ScalarField.hpp"
+#include "fields/VectorField.hpp"
 
 class LinearInterpolator : public Interpolator
 {
 public:
+
     double interpolate(
         const ScalarField& field,
         const Point& position
     ) const override;
+
+    Vector interpolate(
+        const VectorField& field,
+        const Point& position
+    ) const override;
+
+private:
+
+    double interpolate1D(
+        const ScalarField& field,
+        const Point& position
+    ) const;
+
+    Vector interpolate1D(
+        const VectorField& field,
+        const Point& position
+    ) const;
+
+    double interpolate2D(
+        const ScalarField& field,
+        const Point& position
+    ) const;
+
+    Vector interpolate2D(
+        const VectorField& field,
+        const Point& position
+    ) const;
 };
