92fbe4682e5eb7cb06b70b291158dbc1e4bb4d39
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-27 10:16:42 -0700

    Update MCLimiter.cpp

--- discretization/reconstructors/tvd/MCLimiter.cpp
+++ discretization/reconstructors/tvd/MCLimiter.cpp
@@ -4,14 +4,5 @@
 
 double MCLimiter::limit(double r) const
 {
-    return std::max(
-        0.0,
-        std::min(
-            {
-                2.0 * r,
-                0.5 * (1.0 + r),
-                2.0
-            }
-        )
-    );
+    return std::max( 0.0, std::min( { 2.0 * r, 0.5 * (1.0 + r), 2.0 } ) );
 }
