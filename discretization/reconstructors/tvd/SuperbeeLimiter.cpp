bf73c296da980626ccc9095063457dd517db2d2c
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-27 10:17:05 -0700

    Update SuperbeeLimiter.cpp

--- discretization/reconstructors/tvd/SuperbeeLimiter.cpp
+++ discretization/reconstructors/tvd/SuperbeeLimiter.cpp
@@ -4,7 +4,5 @@
 
 double SuperbeeLimiter::limit(double r) const
 {
-    return std::max({0.0, 
-                     std::min(1.0, 2*r), 
-                     std::min(r,2.0)});
+    return std::max({0.0, std::min(1.0, 2*r),  std::min(r,2.0)});
 }
