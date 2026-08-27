f4ee82b8045b3bd6f4ee724ea967a89ef62f7380
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-27 10:20:56 -0700

    Update CSVWriter.cpp

--- io/CSVWriter.cpp
+++ io/CSVWriter.cpp
@@ -12,30 +12,18 @@ void CSVWriter::write(
     const OutputData& data,
     const std::filesystem::path& filename)
 {
-    if (filename.has_parent_path())
-    {
-        std::filesystem::create_directories(
-            filename.parent_path());
-    }
+    if (filename.has_parent_path()) { std::filesystem::create_directories(filename.parent_path()); }
 
     std::ofstream f(filename);
 
-    if (!f.is_open())
-    {
-        throw std::runtime_error(
-            "Failed to open CSV output: " + filename.string());
-    }
+    if (!f.is_open()) { throw std::runtime_error("Failed to open CSV output: " + filename.string()); }
     
     f << std::setprecision(16);
     f << "x,y,z,phi,rhs,residual\n";
 
     const auto& field = *data.reconstructedField;
 
-    if (!data.reconstructedField)
-    {
-        throw std::runtime_error(
-            "CSVWriter requires reconstructed point field");
-    }
+    if (!data.reconstructedField) { throw std::runtime_error("CSVWriter requires reconstructed point field"); }
 
     const std::size_t N = field.phi.size();
 
@@ -43,17 +31,15 @@ void CSVWriter::write(
     {
         const Point& p = field.x[i];
 
-        const double rhs_i =
-            (i < data.rhs.size()) ? data.rhs[i] : 0.0;
+        const double rhs_i = (i < data.rhs.size()) ? data.rhs[i] : 0.0;
 
-        const double res_i =
-            (i < data.residual.size()) ? data.residual[i] : 0.0;
+        const double res_i = (i < data.residual.size()) ? data.residual[i] : 0.0;
 
         f << p.x[0] << ","
-        << p.x[1] << ","
-        << p.x[2] << ","
-        << field.phi[i] << ","
-        << rhs_i << ","
-        << res_i << "\n";
+          << p.x[1] << ","
+          << p.x[2] << ","
+          << field.phi[i] << ","
+          << rhs_i << ","
+          << res_i << "\n";
     }
 }
