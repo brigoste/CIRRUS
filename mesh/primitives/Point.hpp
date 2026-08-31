8703808479e0cc578c6a3f93bbf654e1c2cb973d
Brigham Ostergaard <76854192+brigoste@users.noreply.github.com>
2026-08-31 14:57:23 -0700

    Update Point.hpp

--- mesh/primitives/Point.hpp
+++ mesh/primitives/Point.hpp
@@ -16,33 +16,23 @@ struct Point
 // Point - Point = Vector
 inline Vector operator-(const Point& a, const Point& b)
 {
-    return Vector{
-        a.x[0] - b.x[0],
-        a.x[1] - b.x[1],
-        a.x[2] - b.x[2]
-    };
+    return Vector{ a.x[0] - b.x[0],
+                   a.x[1] - b.x[1],
+                   a.x[2] - b.x[2] };
 }
 
 // Point + Vector = Point
 inline Point operator+(const Point& p, const Vector& v)
 {
-    return Point{
-        {
-            p.x[0] + v.x,
-            p.x[1] + v.y,
-            p.x[2] + v.z
-        }
-    };
+    return Point{ { p.x[0] + v.x,
+                    p.x[1] + v.y,
+                    p.x[2] + v.z } };
 }
 
 // Point - Vector = Point
 inline Point operator-(const Point& p, const Vector& v)
 {
-    return Point{
-        {
-            p.x[0] - v.x,
-            p.x[1] - v.y,
-            p.x[2] - v.z
-        }
-    };
+    return Point{ { p.x[0] - v.x,
+                    p.x[1] - v.y,
+                    p.x[2] - v.z } };
 }
